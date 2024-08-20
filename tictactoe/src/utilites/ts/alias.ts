f13f238814fd93bc12ee9553fc6d91daa1783988
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-08-20 14:34:20 +0200

    utilites

---
+++ tictactoe/src/utilites/ts/alias.ts
@@ -0,0 +1,10 @@
+﻿export type Player = 2 | 0 | 1 //icon of box component
+export type States = 'empty' | 'selected' | 'win' | 'winner' //states of box component
+
+export type Result = 'winner' | 'draw' //resultados para el anuncio de Reuslt componente
+
+export type StatesPlay = 'play' | Result //estados para el anuncio de la partida en board componente
+export type Case = {
+  player : Player,
+  state : States,
+}
---
+++ tictactoe/src/utilites/ts/interfaces.ts
@@ -0,0 +1,16 @@
+﻿import { StatesPlay, Player } from "./alias";
+
+export interface TypeEndGame {
+  type  : 'none' | 'rows' | 'columns' | 'diagonals' | 'draw',
+  place : number
+}
+
+export interface ResultPlay {
+  state: StatesPlay ,
+  player : Player,
+  typeEndGame? : TypeEndGame
+} //resultados para el anuncio de la partida en board componente
+
+export interface Score {
+  score : [number, number]
+}
