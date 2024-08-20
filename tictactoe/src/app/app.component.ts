351ff29dea93f2034965ef775ec74108b9c268d1
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-08-20 14:37:03 +0200

    changes app component

--- tictactoe/src/app/app.component.ts
+++ tictactoe/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SampleComponent } from './3_organisms/sample/sample.component';
+import { TictactoeComponent } from './3_organisms/tictactoe/tictactoe.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SampleComponent,
+    TictactoeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'tictactoe';
+
 }
